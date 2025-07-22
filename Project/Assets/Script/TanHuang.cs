85a9bc7589847dd2fb56840e64181c775f01f513
muyu <2378663702@qq.com>
2025-07-22 09:59:21 +0800

    修改翘班

--- Project/Assets/Script/TanHuang.cs
+++ Project/Assets/Script/TanHuang.cs
@@ -8,10 +8,14 @@ public class TanHuang : MonoBehaviour
     private float rotate = 0;
     private  float lastRotation = 0;
     bool canRotate = true;
+    bool reback = false;
     public GameObject player;
+    public float time = 0f;
+    public Vector3 startRotate;
+    private Rigidbody2D rb;
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -28,9 +32,30 @@ public class TanHuang : MonoBehaviour
             canRotate = false;
         }
 
-        if (rotate < 10)
+        if (rotate < 30)
         {
             canRotate = true;
+        }
+
+        if (rotate > 80 && !reback)
+        {
+            reback = true;
+            startRotate = gameObject.transform.eulerAngles;
+            gameObject.transform.eulerAngles = startRotate;
+        }
+
+        if (reback)
+        {
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = 0;
+            time += Time.deltaTime * 10;
+            gameObject.transform.eulerAngles = Vector3.Lerp(startRotate, new Vector3(0,0,0), time/3f);
+            if (time >= 3f)
+            {
+                reback = false;
+                time = 0;
+            }
+            Debug.Log(time);
         }
     }
 }
